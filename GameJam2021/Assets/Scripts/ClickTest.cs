be12974d444f135147b090d98cbfcefd7aa016d1
jbonto <jrbonto1002@gmail.com>
2021-01-30 17:34:18 -0500

    added alot

---
+++ GameJam2021/Assets/Scripts/ClickTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTest : MonoBehaviour
+{
+    public int mouseInput = -2, mouseSelect;
+    private bool glowing = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(mouseSelect)){
+            mouseInput =1;
+            
+        } 
+        if(Input.GetMouseButtonUp(mouseSelect)){
+            mouseInput = 2;
+            glowing = true;
+
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if(mouseInput == 1 && glowing){
+            Debug.Log("hi");
+            glowing=false;
+        }
+        //Debug.Log("over");
+    }
+}
---
+++ GameJam2021/Assets/Scripts/HoverOver.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverOver : MonoBehaviour
+{
+    public int mouseInput = -2, mouseSelect;
+    public bool good;
+    public GameObject objective;
+    private bool glowing = true, moving = false, acting = false;
+    private Vector2 left, center, right, up;
+    public float distance, benevspeed, evilspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        left = new Vector2(this.transform.position.x - distance, this.transform.position.y);
+        center = new Vector2(this.transform.position.x, this.transform.position.y);
+        right =  new Vector2(this.transform.position.x + distance, this.transform.position.y);
+        up = new Vector2(this.transform.position.x, this.transform.position.y + 6f);
+    }
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(mouseSelect)){
+            mouseInput =1;
+            
+        } 
+        if(Input.GetMouseButtonUp(mouseSelect)){
+            mouseInput = 2;
+            glowing = true;
+
+        }
+    }
+    
+    void OnMouseOver()
+    {
+        if(!moving){
+            StartCoroutine(BenevolentMove());
+            moving = true;
+        }
+        if(mouseInput == 1 && !acting){
+            StartCoroutine(MoveUp());
+            acting = true;
+        }
+        //Debug.Log("over");
+    }
+    IEnumerator BenevolentMove(){
+        //rightthanleft
+        while(this.transform.position.x!=right.x){
+            yield return null;
+            transform.position = Vector2.MoveTowards(transform.position, right, benevspeed);
+            if(acting)
+                break;
+        }
+        while(this.transform.position.x!=left.x){
+            yield return null;
+            transform.position = Vector2.MoveTowards(transform.position, left, benevspeed);
+            if(acting)
+                break;
+
+        }
+        while(this.transform.position.x!=center.x){
+            yield return null;
+            transform.position = Vector2.MoveTowards(transform.position, center, benevspeed);
+            if(acting)
+                break;
+        }
+        moving = false;
+    }
+
+    IEnumerator MoveUp(){
+        while(this.transform.position.y!= up.y){
+            yield return null;
+            transform.position = Vector2.MoveTowards(transform.position, up, benevspeed * 3f);
+        }
+        yield return new WaitForSeconds(4f);
+        while(this.transform.position.y!= center.y){
+            yield return null;
+            transform.position = Vector2.MoveTowards(transform.position, center, benevspeed * 3f);
+        }
+        acting = false;
+    }
+}
