c8e85d555663ca7cc631251b1151f70145d22865
jbonto <jrbonto1002@gmail.com>
2021-01-29 19:35:46 -0500

    Adjusted packages

---
+++ GameJam2021/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    //the sole purpose of this script is to make sure Vs Code functionality is working
+    public string sample;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        
+    }
+}
