ef2574dc80b60b3c424bebe3641ecbd7b6be8690
jbonto <jrbonto1002@gmail.com>
2021-01-30 20:41:12 -0500

    added a light wave

---
+++ GameJam2021/Assets/Scripts/LightWave.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+public class LightWave : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D _light;
+    public float baseFalloff = 0f, falloffMod, baseF, add;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = this.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        
+        StartCoroutine(LightHum());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        
+       // _light.falloffIntensity = 
+    }
+    
+    IEnumerator LightHum(){
+        float mod = 1f;
+        while(2>1){
+            yield return new WaitForSeconds(.06f);
+            baseFalloff++;
+            if(baseFalloff > 60f){
+                mod*=-1f;
+                baseFalloff = 0f;
+            }
+            _light.intensity+=(baseF*mod);
+        }
+    }
+}
